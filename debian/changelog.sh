4112438e59bd037742a1fbc30e9b65d9efe38888
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-08-04 19:27:02 +0200

    Updated more repo urls, also s/mapcrafter/Mapcrafter/

--- debian/changelog.sh
+++ debian/changelog.sh
@@ -4,7 +4,7 @@ VERSION=$(git describe | grep -Po "\d\.\d(\.\d)?(-\d+)?")
 GITVERSION=$(git describe)
 DATE=$(date -R)
 
-echo "mapcrafter ($VERSION) unstable; urgency=low"
+echo "Mapcrafter ($VERSION) unstable; urgency=low"
 echo ""
 echo "  * Git version $GITVERSION"
 echo ""
