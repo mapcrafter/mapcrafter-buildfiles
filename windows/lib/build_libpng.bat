f20ed9e6a94f8aa78f35c0222cbc60345cdc303a
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-08-10 12:49:18 +0200

    Trying to use msys makefiles instead of mingw ones

--- windows/lib/build_libpng.bat
+++ windows/lib/build_libpng.bat
@@ -1,6 +1,6 @@
 cd libpng-1.5.18
 
-cmake -G "MinGW Makefiles" -DZLIB_INCLUDE_DIR=../zlib-1.2.8 -DZLIB_LIBRARY=../zlib-1.2.8/zlib1.dll
+cmake -G "MSYS Makefiles" -DZLIB_INCLUDE_DIR=../zlib-1.2.8 -DZLIB_LIBRARY=../zlib-1.2.8/zlib1.dll
 
 mingw32-make clean
 mingw32-make
