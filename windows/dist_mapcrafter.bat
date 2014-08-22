8302c298a8b5b34cc55f77b4ee8422dcd68c47d3
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-08-22 16:52:24 +0200

    Don't forget logging.conf in dist_mapcrafter.bat

--- windows/dist_mapcrafter.bat
+++ windows/dist_mapcrafter.bat
@@ -5,6 +5,7 @@ cp mapcrafter/build/src/mapcrafter.exe dist
 cp mapcrafter/build/src/mapcrafter_markers.exe dist
 cp mapcrafter/build/src/tools/testtextures.exe dist
 cp mapcrafter/build/src/mapcraftercore/libmapcraftercore.dll dist
+cp mapcrafter/src/logging.conf dist
 
 cp lib/libpng-1.5.18/libpng15.dll dist
 cp lib/zlib-1.2.8/zlib1.dll dist
